d28856fac0e17198c60152a9f5a92ad36db48848
FRANCISC0 RODRIGUEZ <abj951122@gmail.com>
2020-04-04 19:42:55 -0600

    version 1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import DatePicker from 'react-datepicker';
+import DatePicker, {registerLocale} from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import es from 'date-fns/locale/es';
+registerLocale("es", es);
 
 class App extends Component {
 state={
@@ -15,7 +17,8 @@ onChange=fecha=>{
 }
 
 mostrarFecha = fecha=>{
-  alert(fecha);
+  const options = {weekday: 'short', year: 'numeric', month: 'long', day: 'numeric'};
+  alert(fecha.toLocaleDateString('es-ES', options));
 }
 
   render() {
@@ -23,9 +26,8 @@ mostrarFecha = fecha=>{
       <>
         <div className="contenedor">
           <div className="center">
-           <DatePicker selected={this.state.fecha} onChange={this.onChange}/>
+           <DatePicker selected={this.state.fecha} onChange={this.onChange} locale="es" className="pickers" dateFormat="dd 'de' MMMM 'de' yyyy"/>
            <br /><br />
-
            <input type="button" value="Mostrar Fecha" className="btn btn-primary" onClick={()=>this.mostrarFecha(this.state.fecha)}/>
           </div>
         </div>
